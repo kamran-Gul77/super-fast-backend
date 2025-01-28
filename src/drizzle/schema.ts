911c4e7bb869f41711a3ced66b658d43750d0eb3
kamran <wpwizard777@gmail.com>
2025-01-28 22:32:00 +0500

    add basic code

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,8 +1,8 @@
-import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+// import { boolean, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const userTables = pgTable("user", {
-  id: uuid("id").defaultRandom().primaryKey(),
-  email: varchar("email", { length: 255 }).unique().notNull(),
-  userName: varchar("userName", { length: 255 }).notNull(),
-  status: boolean("status").default(false).notNull(),
-});
+// export const userTables = pgTable("user", {
+//   id: uuid("id").defaultRandom().primaryKey(),
+//   email: varchar("email", { length: 255 }).unique().notNull(),
+//   userName: varchar("userName", { length: 255 }).notNull(),
+//   status: boolean("status").default(false).notNull(),
+// });
